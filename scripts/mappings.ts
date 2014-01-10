0b7a529d60493019935abec5517145f10b3a3b21
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-10 10:56:13 -0200

    mapeamento inicial

--- scripts/mappings.ts
+++ scripts/mappings.ts
@@ -18,9 +18,21 @@ match($status) {
     log("--> STATUS: 200")
 
     match($path) {
-      with(/^\/$|^\/\?/) {
+      with(/handler\.php\?module=site&action=view&$/) {
         log("--> Importing pages/home.ts in mappings.ts")
         @import pages/home.ts
+      }
+      with(/handler\.php\?module=site&action=view&#buscar|handler\.php\?module=site&action=view&#afinidade/) {
+        log("--> Importing pages/category.ts in mappings.ts")
+        @import pages/product.ts
+      }
+      with(/handler\.php\?module=site&action=view&#produto/) {
+        log("--> Importing pages/product.ts in mappings.ts")
+        @import pages/product.ts
+      }
+      with(/handler\.php\?module=site&action=view&section=pedido/) {
+        log("--> Importing pages/cart.ts in mappings.ts")
+        @import pages/cart.ts
       }
       else() {
         log("--> No page match in mappings.ts")
