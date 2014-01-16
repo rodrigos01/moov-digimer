ea7be098f0b16e422b8f2b32f5bd03fefd57893b
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-16 17:54:38 -0200

    header

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,1 +1,12 @@
 /* Default JS */
+
+var $j = jQuery.noConflict();
+
+function listeners() {
+	console.debug($j("a.icons-user"));
+	$j("a.icons-user").click(function(e) {
+		e.preventDefault();
+		$j(".loginBox").toggle('fold');
+		e.stopPropagation();
+	});
+}
--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,10 +1,31 @@
 $('./body') {
   insert_top("header", class: "_header") {
-    insert("div", class: "login") {
-    	move_here("//form[@name='user']");
+
+  	insert("a", class: "sprites-logo", href: "/", title: "home");
+
+  	insert("a", class: "button icons-user", href: "#", title: "Usuário");
+
+  	insert("a", class: "button icons-cart", href: "https://www.digimer.com.br/handler.php?module=site&action=view&section=pedido", title: "carrinho de compras");
+
+    insert("div", class: "loginBox") {
+    	move_here("//form[@name='user']") {
+    		remove(".//img");
+    		remove(".//a");
+    		$(".//input[@class='login']") {
+    			insert_before("label", "Login");
+    			insert_after("br");
+    		}
+    		$(".//input[@class='pass']") {
+    			insert_before("label", "Senha");
+    			insert_after("br");
+    		}
+    		insert("input", type: "submit", value: "Ok");
+    	}
     }
   }
 
-  #remove(".//p");
+  insert_javascript_at("bottom", "listeners()");
+
+  remove(".//p");
 }
 
