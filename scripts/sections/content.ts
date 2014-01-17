eed9f33a5d3597e8aa01dfcb7343bc949f22fd7f
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-17 16:26:59 -0200

    melhorias responsivo

--- scripts/sections/content.ts
+++ scripts/sections/content.ts
@@ -1,13 +1,18 @@
 $('./body/header') {
   insert_after("div", class: "_content") {
 
-    insert("a", class: "button icons-menu", href: "#", title: "Menu");
+    insert("div", class: "pannel") {
 
-    move_here("//form[@name='busca']"){
-      $('.//input') {
-        attribute("placeholder", "busca");
+      insert("a", class: "button icons-menu", href: "#", title: "Menu");
+
+      move_here("//form[@name='busca']"){
+        $('.//input') {
+          attribute("placeholder", "busca");
+        }
       }
+      
     }
+
   	move_here("//div[@id='central']") {
       $("div[@id='listagem']/table[1]") {
         attribute("width", "100%");
