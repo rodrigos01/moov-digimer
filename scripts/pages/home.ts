9a288c941ee7f1ba431fa2bf45185754c494cc3d
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-23 12:05:09 -0200

    melhorias destaques

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -15,6 +15,20 @@ $$("body") {
 	    }
 	    insert("div", class: "controles") {
 	      move_here("../../div[@id='listagem']/table[1]/tr[3]/td[2]/table/tr[1]/td[2]/span/*");
+	      insert("div", class: "controlesContainer") {
+	        move_here("../table//a[@id='destaque_site_nav1']") {
+	        	remove("./img");
+	        	attribute("class", "nav icons-navback");
+	        }
+	      	move_here("../table//span") {
+		    	remove("@style");
+		    }
+		    move_here("../table//a[@id='destaque_site_nav2']") {
+		    	remove("./img");
+	        	attribute("class", "nav icons-navnext");
+		    }
+	 	  }
+	      remove("./table");
 	    }
 	  }
 
