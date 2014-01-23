57800894cdf8c60c3872559a5de79b253c26c27c
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-23 10:32:19 -0200

    correcao de assets e icones mobile

--- scripts/sections/head.ts
+++ scripts/sections/head.ts
@@ -5,9 +5,9 @@ $("./head") {
 	insert("meta", name: "mobile-web-app-capable", content: "yes");
 	insert("meta", name: "apple-mobile-web-app-capable", content: "yes");
 
-	insert("link", rel: "shortcut icon", sizes: "196x196", href: "_moovweb_local_assets_/images/touchicon_highres.png");
-	insert("link", rel: "shortcut icon", sizes: "128x128", href: "_moovweb_local_assets_/images/touchicon.png");
-	insert("link", rel: "apple-touch-icon", sizes: "128x128", href: "_moovweb_local_assets_/images/touchicon.png");
-	insert("link", rel: "apple-touch-icon-precomposed", sizes: "128x128", href: "_moovweb_local_assets_/images/touchicon.png");
-	insert("link", rel: "icon", type: "image/x-icon", href: "_moovweb_local_assets_/images/favicon.ico");
+	insert("link", rel: "shortcut icon", sizes: "196x196", href: asset("images/touchicon_highres.png"));
+	insert("link", rel: "shortcut icon", sizes: "128x128", href: asset("images/touchicon.png"));
+	insert("link", rel: "apple-touch-icon", sizes: "128x128", href: asset("images/touchicon.png"));
+	insert("link", rel: "apple-touch-icon-precomposed", sizes: "128x128", href: asset("images/touchicon.png"));
+	insert("link", rel: "icon", type: "image/x-icon", href: asset("images/favicon.ico"));
 }
