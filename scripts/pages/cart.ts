dc17abf811be451fbd359670bbbfcc341115c163
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-01-27 18:24:29 -0200

    dia 27/01/2014

--- scripts/pages/cart.ts
+++ scripts/pages/cart.ts
@@ -1,4 +1,13 @@
 $("//div[@id='central']") {
-	@import ../sections/central.ts
-	remove("./table[2]/following-sibling::table");
+
+	add_class("_cart")
+
+	move_here("./table[@width='500']//td[@align='center']/div", "top");
+
+	insert_at("top", "h1", "Carrinho de Compras", class: "title icons-cart");
+	remove("./table[1]");
+
+
+
+	#remove("./table[2]/following-sibling::table");
 }
--- scripts/sections/central.ts
+++ scripts/sections/central.ts
@@ -2,5 +2,7 @@
 
   $(".//div[@class='lista_produto']") {
   	move_here("self/preceding-sibling::form");
+  	move_here("self/following-sibling::a");
+  	remove("self/following-sibling::br");
     insert("div", class: "border");
   }
